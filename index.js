79d6de773f753cb9fa67f8c7fcb8213d37be2097
Amit <amitsarker89@gmail.com>
2023-01-23 23:50:36 +0100

    remove lines

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
 "strict mode";
-let local_new = "abcog new";
-let local_abcde = "local abcd";
-let local_abcdf = "local abcd";
-let local_abcdg = "local abcd";
-let local_abco = "local abco";
-let local_abcog = "local abcog new";
+let stg_new = "stage change";
+
