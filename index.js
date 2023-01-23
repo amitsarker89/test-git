c748489c2afc731dc569016e6c912db3b569ff2d
Amit <amitsarker89@gmail.com>
2023-01-23 23:40:37 +0100

    add new lines

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 "strict mode";
 let abc = "123";
-let local_abcdh = "local abcd";
+let local_abcdh = "local abcd new";
 let local_abcde = "local abcd";
 let local_abcdf = "local abcd";
 let local_abcdg = "local abcd";
 let local_abco = "local abco";
+let local_abcog = "local abcog new";
