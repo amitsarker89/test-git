2a917ef78ff12f8bf50934d389e66e2a28c9928b
Amit Sarker <amitsarker89@gmail.com>
2023-01-23 23:42:23 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 "strict mode";
-let abc = "123";
-let local_abcdh = "local abcd new";
+let abc = "1234";
 let local_abcde = "local abcd";
 let local_abcdf = "local abcd";
 let local_abcdg = "local abcd";
